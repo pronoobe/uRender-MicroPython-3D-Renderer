a31bb7272eecc7a2e4f6319fa6d6b1fe06300755
pronoobe <1781110829@qq.com>
2023-12-05 15:57:44 +0800

    Update uRender.py

--- uRender.py
+++ uRender.py
@@ -1,48 +1,68 @@
-from GR import *  # Importing all from the GR module
+from GR import *
 from math import acos, sqrt, pi
 
-# Function to convert degree angles to radians for x, y, z
+# Function to convert angles from degrees to radians
 def get_pos_vec(x, y, z):
-    return list((x/180*pi, y/180*pi, z/180*pi))
+    return list((x / 180 * pi, y / 180 * pi, z / 180 * pi))
 
 class MicroRender:
-    # Initialization of the MicroRender class
     def __init__(self, camera_pos=[0, 0, 0], rotation_angles=[0, 0, 0], screen_x=128, screen_y=64):
-        self.renderer = GraphRender(camera_pos, rotation_angles)  # Creating a GraphRender object
+        # Initialize the renderer with camera position, rotation angles, and screen dimensions
+        self.renderer = GraphRender(camera_pos, rotation_angles)
         self.camera_pos = camera_pos
         self.rotation_angles = rotation_angles
-        self.screen_x = screen_x  # Width of the screen
-        self.screen_y = screen_y  # Height of the screen
-        self.lines = []  # List to store lines
+        self.screen_x = screen_x
+        self.screen_y = screen_y
+        self.lines = []
 
-    # Method to render points and lines on the screen
-    def rending(self, screen, points, line=False, show_index=False):
+    # Method to render points using orthogonal projection
+    def rending_ord(self, screen, points, normal_vec, line=False, show_index=False):
         try:
-            screen.fill(0)  # Clearing the screen
-            # Projecting 3D points onto a 2D plane
-            projected_points_graph_render = self.renderer.project_points(points)
-            # Scaling points to fit the screen
-            show_points = self.renderer.scale_to_screen(self.screen_x, self.screen_y, projected_points_graph_render)
+            screen.fill(0)
+            projected_points = self.renderer.project_points_ord(points, normal_vec)
+            display_points = self.renderer.scale_to_screen(self.screen_x, self.screen_y, projected_points)
 
-            # Drawing points and optionally their indices
-            for point in show_points:
+            # Display points and optionally their indices
+            for point in display_points:
                 screen.ellipse(int(point[0]), int(point[1]), 2, 2, 1, not line)
                 if show_index:
-                    screen.text(str(show_points.index(point) + 1), int(point[0]), int(point[1]))
+                    screen.text(str(display_points.index(point) + 1), int(point[0]), int(point[1]))
 
-            # Drawing lines if the line option is True
+            # Draw lines between points if specified
             if line:
-                for point1i in range(len(show_points)):
-                    for point2i in range(len(show_points)):
-                        if (point1i + 1, point2i + 1) in self.lines:
-                            point1 = show_points[point1i]
-                            point2 = show_points[point2i]
-                            x1, y1 = point1[0], point1[1]
-                            x2, y2 = point2[0], point2[1]
+                for i in range(len(display_points)):
+                    for j in range(len(display_points)):
+                        if (i + 1, j + 1) in self.lines:
+                            x1, y1 = display_points[i][0], display_points[i][1]
+                            x2, y2 = display_points[j][0], display_points[j][1]
                             screen.line(int(x1), int(y1), int(x2), int(y2), 1)
-            screen.show()  # Displaying the rendered content
+            screen.show()
         except ValueError:
-            print("error rending")
+            print("error rendering")
+
+    # Method to render points using central projection
+    def rending_center(self, screen, points, line=False, show_index=False):
+        try:
+            screen.fill(0)
+            projected_points = self.renderer.project_points_center(points)
+            display_points = self.renderer.scale_to_screen(self.screen_x, self.screen_y, projected_points)
+
+            # Similar rendering logic as in rending_ord
+            for point in display_points:
+                screen.ellipse(int(point[0]), int(point[1]), 2, 2, 1, not line)
+                if show_index:
+                    screen.text(str(display_points.index(point) + 1), int(point[0]), int(point[1]))
+
+            if line:
+                for i in range(len(display_points)):
+                    for j in range(len(display_points)):
+                        if (i + 1, j + 1) in self.lines:
+                            x1, y1 = display_points[i][0], display_points[i][1]
+                            x2, y2 = display_points[j][0], display_points[j][1]
+                            screen.line(int(x1), int(y1), int(x2), int(y2), 1)
+            screen.show()
+        except ValueError:
+            print("error rendering")
 
     # Method to set the camera position
     def set_camera_pos(self, camera_pos):
@@ -53,5 +73,7 @@ class MicroRender:
         self.renderer.rotation_angles = rotation_angles
         self.renderer.rotation_matrix = self.renderer.create_rotation_matrix(rotation_angles)
 
-    # Method to add a line between two points
-   
+    # Method to define lines between points
+    def add_line(self, p1, p2):
+        self.lines.append((p1, p2))
+
